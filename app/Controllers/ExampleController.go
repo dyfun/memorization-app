f63f782e29e0e392467a4079e7c340c9249f9ea8
Tayfun Güler <gulertayfun@outlook.com>
2023-06-17 20:43:56 +0300

    added new example

--- app/Controllers/ExampleController.go
+++ app/Controllers/ExampleController.go
@@ -2,8 +2,8 @@ package Controllers
 
 import "github.com/gofiber/fiber/v2"
 
-func Index(c *fiber.Ctx) error {
+func Example(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"message": "Hello, World! This is Fiber!aa",
+		"message": "Example Controller",
 	})
 }
