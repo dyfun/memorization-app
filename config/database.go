bec65d5d9cda4e94eddabe19ebec6f6484806dc1
Tayfun Güler <gulertayfun@outlook.com>
2023-06-21 00:19:52 +0300

    database connection error fixed

--- config/database.go
+++ config/database.go
@@ -2,20 +2,20 @@ package config
 
 import (
 	"fmt"
+	"github.com/dyfun/memorization-app/app/Models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"os"
 )
 
-type Database struct {
-	DB *gorm.DB
-}
+var Db *gorm.DB
 
-func Connect() Database {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
+func Connect() *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
@@ -24,10 +24,12 @@ func Connect() Database {
 	})
 
 	if err != nil {
-		panic("failed to connect database")
+		panic(err.Error())
 	}
 
-	return Database{
-		DB: db,
-	}
+	// Migrate the schema
+	db.AutoMigrate(&Models.User{})
+
+	Db = db
+	return nil
 }
